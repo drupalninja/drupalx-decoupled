f6085935cf2cc011de1ad61503c7d4ba90f2d9b1
Jay Callicott <jrcallicott@gmail.com>
2024-07-26 15:34:28 -0500

    Added missing image style

--- nextjs/graphql/fragments/media.ts
+++ nextjs/graphql/fragments/media.ts
@@ -9,7 +9,7 @@ export const ImageFragment = graphql(`
     height
     alt
     title
-    variations(styles: [HERO_L_X2, HERO_S, I4_3_SMALL, I16_9_LARGE_2X, LARGE]) {
+    variations(styles: [HERO_L_X2, HERO_S, I4_3_SMALL, I4_3_LARGE_2X, I16_9_LARGE_2X, LARGE]) {
       name
       url
       width
