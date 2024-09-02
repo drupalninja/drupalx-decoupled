6bd25e97d0ba78b40ad653182b965b49a4f60fab
Jay Callicott <jrcallicott@gmail.com>
2024-09-02 18:02:33 -0500

    Updated header

--- nextjs/components/main-menu/MainMenu.tsx
+++ nextjs/components/main-menu/MainMenu.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { Navbar, Nav, Offcanvas, Container, NavDropdown } from 'react-bootstrap';
+import Button from '../button/Button';
 import { MainMenuItem, MainMenuProps } from './Types';
 import { usePathname } from 'next/navigation';
 import { frontpagePath } from '@/utils/routes';
@@ -16,11 +17,13 @@ const MainMenu: React.FC<MainMenuProps> = ({
   siteName,
   showLogo,
   menuItems,
+  ctaLinkCount,
 }) => {
   const pathname = usePathname();
 
   // Add active trail classes to menu items.
-  menuItems = menuItems.map((item) => {
+  menuItems = menuItems.map((item, index) => {
+    const isCTA = index >= menuItems.length - ctaLinkCount;
     if (item.url === frontpagePath && pathname === '/') {
       item.inActiveTrail = true;
     }
@@ -33,11 +36,11 @@ const MainMenu: React.FC<MainMenuProps> = ({
         return subItem;
       });
     }
-    return item;
+    return { ...item, isCTA };
   });
 
   return (
-    <Navbar bg="primary" expand="lg" variant="dark" className={`${modifier}`}>
+    <Navbar bg="white" expand="lg" className={`${modifier} border-bottom`}>
       <Container>
         <Navbar.Brand href="/" className={!showLogo ? "py-4 fs-2 fw-bold" : ""}>
           {showLogo && <Image src={siteLogo ?? ''} alt="Site Name" width={312} height={96} style={{ marginRight: '4px' }} />}
@@ -55,13 +58,13 @@ const MainMenu: React.FC<MainMenuProps> = ({
           </Offcanvas.Header>
           <Offcanvas.Body>
             <Nav className="justify-content-end flex-grow-1 pe-3">
-              <MainMenuItems items={menuItems} linkModifier={linkModifier} depth={0} isDesktop={false} />
+              <MainMenuItems items={menuItems} linkModifier={linkModifier} depth={0} ctaLinkCount={ctaLinkCount} isDesktop={false} />
             </Nav>
           </Offcanvas.Body>
         </Navbar.Offcanvas>
         <Navbar.Collapse id="navbarSupportedContent" className="d-none d-lg-block">
-          <Nav className="ms-auto gap-4">
-            <MainMenuItems items={menuItems} linkModifier={linkModifier} depth={0} isDesktop={true} />
+          <Nav className="ms-auto gap-3 align-items-center">
+            <MainMenuItems items={menuItems} linkModifier={linkModifier} depth={0} ctaLinkCount={ctaLinkCount} isDesktop={true} />
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -70,41 +73,57 @@ const MainMenu: React.FC<MainMenuProps> = ({
 };
 
 const MainMenuItems: React.FC<{
-  items: MainMenuItem[];
+  items: (MainMenuItem & { isCTA?: boolean })[];
   linkModifier?: string;
   depth: number;
   isDesktop: boolean;
-}> = ({ items, linkModifier, depth, isDesktop }) => {
+  ctaLinkCount: number;
+}> = ({ items, linkModifier, depth, isDesktop, ctaLinkCount }) => {
   return (
     <>
-      {items.map((item, index) => (
-        item.below ? (
-          <NavDropdown
-            key={index}
-            title={item.title}
-            id={`nav-dropdown-${index}`}
-            className={`fs-5 ${linkModifier ?? ''} ${item.inActiveTrail ? 'active' : ''}`}
-          >
-            {item.below.map((subItem, subIndex) => (
-              <NavDropdown.Item
-                key={subIndex}
-                href={subItem.url}
-                className={`${linkModifier ?? ''} ${subItem.inActiveTrail ? 'active' : ''}`}
-              >
-                {subItem.title}
-              </NavDropdown.Item>
-            ))}
-          </NavDropdown>
-        ) : (
+      {items.map((item, index) => {
+        if (item.isCTA) {
+          return (
+            <Button
+              key={index}
+              url={item.url}
+              text={item.title}
+              modifier={index === items.length - ctaLinkCount ? "btn-primary" : "btn-outline-primary"}
+            />
+          );
+        }
+
+        if (item.below) {
+          return (
+            <NavDropdown
+              key={index}
+              title={item.title}
+              id={`nav-dropdown-${index}`}
+              className={`fs-6 ${linkModifier ?? ''} ${item.inActiveTrail ? 'active' : ''}`}
+            >
+              {item.below.map((subItem, subIndex) => (
+                <NavDropdown.Item
+                  key={subIndex}
+                  href={subItem.url}
+                  className={`${linkModifier ?? ''} ${subItem.inActiveTrail ? 'active' : ''}`}
+                >
+                  {subItem.title}
+                </NavDropdown.Item>
+              ))}
+            </NavDropdown>
+          );
+        }
+
+        return (
           <Link
             key={index}
             href={item.url}
-            className={`nav-link fs-5 ${linkModifier ?? ''} ${item.inActiveTrail ? 'active' : ''}`}
+            className={`nav-link fs-6 ${linkModifier ?? ''} ${item.inActiveTrail ? 'active' : ''}`}
           >
             {item.title}
           </Link>
-        )
-      ))}
+        );
+      })}
     </>
   );
 };
