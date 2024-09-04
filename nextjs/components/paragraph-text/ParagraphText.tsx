3dffd7331ba9820d63440d82381c208380688abc
Jay Callicott <jrcallicott@gmail.com>
2024-09-04 15:49:21 -0500

    Updated paragraph text

--- nextjs/components/paragraph-text/ParagraphText.tsx
+++ nextjs/components/paragraph-text/ParagraphText.tsx
@@ -9,17 +9,17 @@ interface ParagraphTextProps {
   modifier?: string;
 }
 
-export default function ParagraphText({ paragraph, modifier } : ParagraphTextProps) {
+export default function ParagraphText({ paragraph, modifier }: ParagraphTextProps) {
   const { title, body, link } = readFragment(ParagraphTextFragment, paragraph);
   const textSummaryFragment = readFragment(TextSummaryFragment, body)
   const linkFragment = readFragment(LinkFragment, link);
-  
+
   return (
     <div className={modifier ?? 'container my-6 my-lg-15'}>
       <div className={`row ${modifier}`}>
         <div className="col-lg-10 mx-auto">
           {title && (
-            <h2 className={`mb-2 display-3`}>{title}</h2>
+            <h2 className={`mb-2`}>{title}</h2>
           )}
           {body && (
             <div dangerouslySetInnerHTML={{ __html: textSummaryFragment?.value ?? '' }}></div>
