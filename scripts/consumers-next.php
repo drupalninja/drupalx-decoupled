690cbf354383fbe30176308cb9ddd748d3edbcb8
Jay Callicott <jrcallicott@gmail.com>
2024-09-09 07:38:34 -0500

    Removed guardr_core and cleaned up folder that keeps getting created

--- scripts/consumers-next.php
+++ scripts/consumers-next.php
@@ -128,3 +128,8 @@ else {
     \Drupal::messenger()->addWarning($message);
   }
 }
+
+// Small cleanup to delete erroneous folder.
+if (file_exists('public:') && is_writable('public:')) {
+  rmdir('public:');
+}
