adffe8c1e7403684b47d437eae153b924c843347
Jay Callicott <jrcallicott@gmail.com>
2024-09-12 15:06:21 -0500

    Fixed storybook preview issues

--- nextjs/.storybook/main.ts
+++ nextjs/.storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
 
 const config: StorybookConfig = {
   stories: [
@@ -11,12 +12,29 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
-    'storybook-addon-sass-postcss',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        postCss: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
   staticDirs: ["../public"],
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@": path.resolve(__dirname, "../"),
+      };
+    }
+    return config;
+  },
 };
+
 export default config;
