8603e5f8aef14b2304cdbea07a00aa93563011dd
Jay Callicott <jrcallicott@gmail.com>
2024-09-12 07:08:01 -0500

    Updated hero, text, side by side

--- nextjs/components/paragraph-sidebyside/ParagraphSidebyside.tsx
+++ nextjs/components/paragraph-sidebyside/ParagraphSidebyside.tsx
@@ -1,11 +1,9 @@
-import classNames from 'classnames';
 import { FragmentOf, readFragment } from 'gql.tada';
 import { TextFragment, LinkFragment } from "@/graphql/fragments/misc";
 import { ParagraphSidebysideFragment } from "@/graphql/fragments/paragraph";
 import { getImage } from '../helpers/Utilities';
-import Badge from '../badge/Badge';
-import Button from '../button/Button';
-import './ParagraphSidebyside.scss';
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 
 interface ParagraphSidebysideProps {
   paragraph: FragmentOf<typeof ParagraphSidebysideFragment>,
@@ -18,25 +16,26 @@ export default function ParagraphSidebyside({ paragraph, modifier }: ParagraphSi
   const textFragment = readFragment(TextFragment, sidebysideSummary);
 
   return (
-    <div className={classNames('side-by-side', layout, modifier ?? 'container my-6 my-lg-15')}>
-      <div className={classNames('row', 'flex-column', 'gy-3', { 'flex-lg-row-reverse': layout === 'right', 'flex-lg-row': layout === 'left' }, 'justify-content-between', 'align-items-center')}>
-        <div className="col-lg-6">
-          {getImage(media, 'img-fluid', ['I43SMALL', 'I43LARGE2X'])}
-        </div>
-        <div className="col-lg-6 col-xxl-5 d-flex flex-column gap-3">
-          {eyebrow && <Badge tag={eyebrow} modifier="text-uppercase" />}
-          <h2 className="mb-2">{sidebysideTitle}</h2>
-          {textFragment && (
-            <div dangerouslySetInnerHTML={{ __html: textFragment?.value ?? '' }}></div>
-          )}
-          {linkFragment?.url && (
-            <Button
-              url={linkFragment?.url}
-              text={linkFragment?.title ?? 'Read more'}
-              modifier="btn-primary"
-            />
-          )}
-        </div>
+    <div className={`flex flex-col lg:flex-row items-center justify-between gap-6 ${modifier ?? 'container my-6 lg:my-15'} ${layout === 'right' ? 'lg:flex-row-reverse' : ''}`}>
+      <div className="w-full lg:w-1/2">
+        {getImage(media, 'w-full h-auto', ['I43SMALL', 'I43LARGE2X'])}
+      </div>
+      <div className="w-full lg:w-1/2 xl:w-5/12 flex flex-col gap-3">
+        {eyebrow && <Badge variant="outline" className="uppercase">{eyebrow}</Badge>}
+        <h2 className="mb-2 text-2xl font-bold">{sidebysideTitle}</h2>
+        {textFragment && (
+          <div dangerouslySetInnerHTML={{ __html: textFragment?.value ?? '' }}></div>
+        )}
+        {linkFragment?.url && (
+          <Button
+            variant="default"
+            asChild
+          >
+            <a href={linkFragment.url}>
+              {linkFragment?.title ?? 'Read more'}
+            </a>
+          </Button>
+        )}
       </div>
     </div>
   );
