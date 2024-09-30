33c1ba40dbac39efc50cad23de98cc9681c1e1c3
Jay Callicott <jrcallicott@gmail.com>
2024-09-30 16:19:13 -0500

    Updated newsletter

--- nextjs/components/newsletter/Newsletter.tsx
+++ nextjs/components/newsletter/Newsletter.tsx
@@ -14,15 +14,15 @@ export default function Newsletter({ title, summary, modifier }: NewsletterProps
   return (
     <div className="bg-gray-100 text-gray-900">
       <div className={`py-8 lg:py-24 ${modifier ?? 'container mx-auto px-8'}`}>
-        <div className="flex flex-col lg:flex-row lg:items-center lg:space-x-8">
-          <div className="lg:w-1/2 flex flex-col justify-center">
+        <div className="flex flex-col lg:space-y-8">
+          <div className="flex flex-col justify-center">
             <h2 className="text-3xl font-semibold mb-4 sm:text-4xl">{title}</h2>
             <div
               dangerouslySetInnerHTML={{ __html: summary }}
               className='newsletter-summary'
             />
           </div>
-          <div className="lg:w-1/2 mt-6 lg:mt-0 flex items-center">
+          <div className="mt-6 lg:mt-0">
             <NewsletterForm />
           </div>
         </div>
@@ -33,7 +33,7 @@ export default function Newsletter({ title, summary, modifier }: NewsletterProps
 
 export function NewsletterForm() {
   return (
-    <div className="w-full">
+    <div className="md:w-3/4 xl:w-1/2">
       <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2">
         <Input
           type="email"
